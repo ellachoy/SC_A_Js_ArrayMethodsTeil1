32ccd0a9fd4122efaae4834286e5f60727acca0b
Ella Choy <egal21de@gmail.com>
2021-08-28 09:51:23 +0200

    Array_slice

--- assets/js/array.js
+++ assets/js/array.js
@@ -235,7 +235,39 @@ console.log(array11);
 array11.push('imageTeilnehmer014supercode.jpg','imageTeilnehmer015supercode.jpg','imageTeilnehmer016supercode.jpg', 'imageTeilnehmer017supercode.jpg','imageTeilnehmer018supercode.jpg', 'imageTeilnehmer019supercode.jpg');
 console.log(array11);
 array11.push('imageTeilnehmer002supercode.jpg','imageTeilnehmer003supercode.jpg','imageTeilnehmer004supercode.jpg', 'imageTeilnehmer005supercode.jpg','imageTeilnehmer006supercode.jpg', 'imageTeilnehmer007supercode.jpg','imageTeilnehmer011supercode.jpg','imageTeilnehmer012supercode.jpg','imageTeilnehmer013supercode.jpg');
+
+array11.sort();
 console.log(array11);
 
-//  frage an Trainern : wie nummerish angezeigt werden ????????????????
 
+console.log('');
+console.log('');
+console.log('**lev1_12: Arrays slice()');
+
+let array12 = [
+    "imageTeilnehmer000supercode.jpg",
+    "imageTeilnehmer001supercode.jpg",
+    "imageTeilnehmer002supercode.jpg",
+    "imageTeilnehmer003supercode.jpg",
+    "imageTeilnehmer004supercode.jpg",
+    "imageTeilnehmer005supercode.jpg",
+    "imageTeilnehmer006supercode.jpg",
+    "imageTeilnehmer007supercode.jpg",
+    "imageTeilnehmer008supercode.jpg",
+    "imageTeilnehmer009supercode.jpg",
+    "imageTeilnehmer010supercode.jpg",
+    "imageTeilnehmer011supercode.jpg",
+    "imageTeilnehmer012supercode.jpg",
+    "imageTeilnehmer013supercode.jpg",
+    "imageTeilnehmer014supercode.jpg",
+    "imageTeilnehmer015supercode.jpg",
+    "imageTeilnehmer016supercode.jpg",
+    "imageTeilnehmer017supercode.jpg",
+    "imageTeilnehmer018supercode.jpg",
+    "imageTeilnehmer019supercode.jpg"
+  ];
+  const copyImg1=array12.slice(7,15);
+  console.log(copyImg1);
+  const copyImg2=array12.slice(6,12);
+  console.log(copyImg2);
+  
